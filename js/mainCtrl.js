0235d76f4f5201bf98cdeacdba2fb3c73410dd37
Steven Anderson <stevenanderson@Stevens-MacBook-Pro.local>
2016-09-26 13:59:19 -0600

    only needs input box

---
+++ js/mainCtrl.js
@@ -0,0 +1,10 @@
+angular.module('friendsList')
+.controller('mainCtrl', function($scope){
+  $scope.friends = [
+    'Brad Trevet',
+    'Brandon Voyles',
+    'Garrett Peterson',
+    'David Lewis',
+    'Brayden Platt'
+  ];
+});
